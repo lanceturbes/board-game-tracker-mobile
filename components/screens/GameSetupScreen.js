36fed90de0a4de25b51b6c0553a161e60d92d2a1
Lance Turbes <50921397+lanceturbes@users.noreply.github.com>
2022-02-23 13:13:22 -0800

    feat: add placeholder for game setup screen

---
+++ components/screens/GameSetupScreen.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+export default function GameSetupScreen() {
+  return (
+    <View>
+      <Text>Game Setup</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
