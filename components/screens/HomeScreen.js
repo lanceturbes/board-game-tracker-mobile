b7bb29194782f5666e571b4cc9502ac80d63d46e
Lance Turbes <50921397+lanceturbes@users.noreply.github.com>
2022-02-23 13:12:52 -0800

    feat: create home screen component

---
+++ components/screens/HomeScreen.js
@@ -0,0 +1,35 @@
+import { StyleSheet, Text, View, Button } from "react-native";
+import React from "react";
+
+export default function HomeScreen(props) {
+  const { navigate } = props.navigation;
+
+  return (
+    <View style={styles.viewContainer}>
+      <Text style={styles.textSubtitle}>Welcome to</Text>
+      <Text style={styles.textTitle}>Monopoly Tracker!</Text>
+      <Button
+        title="Start New Game"
+        color="red"
+        onPress={() => navigate("Setup")}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  viewContainer: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  textTitle: {
+    fontSize: 36,
+    textAlign: "center",
+    marginBottom: 24,
+  },
+  textSubtitle: {
+    fontSize: 24,
+    textAlign: "center",
+  },
+});
