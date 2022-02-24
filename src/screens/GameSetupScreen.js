d655ec037aa684bfd795d2ae5c962fb9a0341eae
Lance Turbes <50921397+lanceturbes@users.noreply.github.com>
2022-02-24 10:13:06 -0800

    feat: add player counter to game setup screen

--- src/screens/GameSetupScreen.js
+++ src/screens/GameSetupScreen.js
@@ -1,16 +1,69 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+import React, { useState } from "react";
 
 export default function GameSetupScreen() {
+  const [playerCount, setPlayerCount] = useState(1);
+
+  const handlePlayerCount = (operation) => {
+    switch (operation) {
+      case "add":
+        setPlayerCount(playerCount + 1);
+        break;
+      case "subtract":
+        // Prevent going below 1
+        if (playerCount - 1 > 0) {
+          setPlayerCount(playerCount - 1);
+        }
+        break;
+      default:
+        console.error(
+          "Invalid operation inputted to 'handlePlayerCount' method in game setup screen.",
+        );
+        break;
+    }
+  };
+
   return (
-    <View>
-      <Text style={styles.textGeneral}>Game Setup</Text>
+    <View style={styles.viewContainerScreen}>
+      <Text style={styles.textGeneral}>How many players?</Text>
+
+      <View style={styles.viewContainerCounter}>
+        <Button
+          style={styles.buttonCounter}
+          title="-1"
+          onPress={() => handlePlayerCount("subtract")}
+        />
+        <Text style={styles.textCounter}>{playerCount}</Text>
+        <Button
+          style={styles.buttonCounter}
+          title="+1"
+          onPress={() => handlePlayerCount("add")}
+        />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  viewContainerScreen: {
+    flex: 1,
+    alignItems: "center",
+  },
+  viewContainerCounter: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    textAlign: "center",
+  },
   textGeneral: {
     color: "black",
+    fontSize: 32,
+  },
+  textCounter: {
+    fontSize: 48,
+  },
+  buttonCounter: {
+    height: 64,
+    width: 64,
   },
 });
