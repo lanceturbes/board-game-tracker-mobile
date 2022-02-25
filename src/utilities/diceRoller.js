e83468b83451392e37c33b58f8c02b0679c75e54
Lance Turbes <50921397+lanceturbes@users.noreply.github.com>
2022-02-25 13:16:14 -0800

    fix: add 1 to dice roll result so range is 1-6

--- src/utilities/diceRoller.js
+++ src/utilities/diceRoller.js
@@ -2,7 +2,7 @@ const getDiceRoll = (diceCount) => {
   const diceRolls = [];
 
   for (let i = 0; i < diceCount; i++) {
-    const randomDiceRoll = Math.floor(Math.random() * 6);
+    const randomDiceRoll = Math.floor(Math.random() * 6) + 1;
     diceRolls.push(randomDiceRoll);
   }
 
