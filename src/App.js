34f16f512350d33cc5c14e21caba0f6ccba75a0d
Lance Turbes <50921397+lanceturbes@users.noreply.github.com>
2022-02-25 11:42:30 -0800

    feat: add safe area provider to app

--- src/App.js
+++ src/App.js
@@ -5,12 +5,15 @@
 
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 import { Navigation } from "./components";
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Navigation />
-    </NavigationContainer>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <Navigation />
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 }
