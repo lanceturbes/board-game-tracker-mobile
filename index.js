211bb160d956f5289f0b5f9f0b36a212e70510bf
Lance Turbes <50921397+lanceturbes@users.noreply.github.com>
2022-02-25 20:55:01 -0800

    refactor: move business logic outside of index.js

--- index.js
+++ index.js
@@ -1,29 +1,6 @@
-import React from "react";
 import { AppRegistry } from "react-native";
-import { DefaultTheme, Provider as PaperProvider } from "react-native-paper";
+
 import App from "./src/App";
 import { name as appName } from "./app.json";
 
-// Global State Management
-import { createStore } from "redux";
-import { Provider as ReduxProvider } from "react-redux";
-import rootReducer from "./src/reducers/rootReducer";
-
-const store = createStore(rootReducer);
-
-// Global Theme Management
-const theme = {
-  ...DefaultTheme,
-};
-
-export default function Main() {
-  return (
-    <ReduxProvider store={store}>
-      <PaperProvider theme={theme}>
-        <App />
-      </PaperProvider>
-    </ReduxProvider>
-  );
-}
-
-AppRegistry.registerComponent(appName, () => Main);
+AppRegistry.registerComponent(appName, () => App);
