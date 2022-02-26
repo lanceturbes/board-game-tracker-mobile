ed8f7d98036e9e26305f408ea879226d20e3b9a1
Lance Turbes <50921397+lanceturbes@users.noreply.github.com>
2022-02-25 20:53:58 -0800

    feat: add dark theme file

---
+++ src/styles/theme-dark.js
@@ -0,0 +1,24 @@
+import configureFonts from "./fonts";
+
+const darkTheme = {
+  dark: true,
+  mode: "adaptive",
+  roundness: 8,
+  colors: {
+    primary: "#fff8e6",
+    accent: "#bfe5d9",
+    background: "#8a624c",
+    surface: "#8a624c",
+    text: "#fff8e6",
+    disabled: "#493928",
+    placeholder: "#88624d",
+    backdrop: "#fff8e6",
+    onSurface: "#282828",
+  },
+  fonts: configureFonts(),
+  animation: {
+    scale: 1.0,
+  },
+};
+
+module.exports = darkTheme;
