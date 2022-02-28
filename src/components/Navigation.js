6d520215522378b2b2adfecb1e56bff9ac28e9ad
Lance Turbes <50921397+lanceturbes@users.noreply.github.com>
2022-02-28 07:44:13 -0800

    fix: remove white flash during screen transitions

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,13 +2,19 @@ import "react-native-gesture-handler";
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { HomeScreen, GameSetupScreen, GamePlayScreen } from "../screens";
+import { darkTheme } from "../styles/theme";
 
 export default function Navigation(props) {
   const { initialRoute } = props;
   const { Navigator, Screen } = createNativeStackNavigator();
 
   return (
-    <Navigator initialRouteName={initialRoute || "Home"}>
+    <Navigator
+      initialRouteName={initialRoute || "Home"}
+      screenOptions={{
+        cardStyle: { backgroundColor: darkTheme.colors.background },
+      }}
+    >
       <Screen name="Home" component={HomeScreen} />
       <Screen name="Setup" component={GameSetupScreen} />
       <Screen name="Play" component={GamePlayScreen} />
