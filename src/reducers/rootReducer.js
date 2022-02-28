5c94f22a83a3e95382f8511f6bb2bd1bc8c2dce9
Lance Turbes <50921397+lanceturbes@users.noreply.github.com>
2022-02-28 09:01:41 -0800

    refactor: separate reducers to individual files

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,55 +1,8 @@
 import { combineReducers } from "redux";
+import { gameOptionsReducer } from "./gameOptionsReducer";
 
-export const initialState = {
-  playerCount: 1,
-  needDice: true,
-  diceCount: 1,
-  scoringSystem: {
-    points: true,
-  },
-};
-
-const gameOptionsReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case "TOGGLE_NEED_DICE":
-      return {
-        ...state,
-        needDice: !state.needDice,
-      };
-
-    case "ADD_PLAYER":
-      return {
-        ...state,
-        playerCount: state.playerCount + 1,
-      };
-
-    case "SUBTRACT_PLAYER":
-      if (state.playerCount - 1 < 1) {
-        return state;
-      } else {
-        return {
-          ...state,
-          playerCount: state.playerCount - 1,
-        };
-      }
-
-    case "SET_DICE_COUNT":
-      return {
-        ...state,
-        diceCount: action.payload,
-      };
-
-    case "SET_SCORING_SYSTEM":
-      return {
-        ...state,
-        scoringSystem: action.payload,
-      };
-
-    default:
-      return state;
-  }
-};
-
-export default combineReducers({
+const rootReducer = combineReducers({
   gameOptions: gameOptionsReducer,
 });
+
+export default rootReducer;
