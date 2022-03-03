4dfbddf438ad02c2b6dda415fc541e101b5394b4
Lance Turbes <50921397+lanceturbes@users.noreply.github.com>
2022-03-03 15:23:20 -0800

    test: use nativebase in specs, test dice toggle

--- src/__tests__/HomeScreen.spec.js
+++ src/__tests__/HomeScreen.spec.js
@@ -1,12 +1,23 @@
 import React from "react";
 import { render, fireEvent } from "@testing-library/react-native";
+import { NativeBaseProvider } from "native-base";
 import { HomeScreen } from "../screens";
 
 // Mock React Navigation
 const navigation = { navigate: jest.fn() };
 
+// Set Screen Dimensions
+const inset = {
+  frame: { x: 0, y: 0, width: 0, height: 0 },
+  insets: { top: 0, left: 0, right: 0, bottom: 0 },
+};
+
 it("renders without errors", () => {
-  const screen = render(<HomeScreen navigation={navigation} />);
+  const screen = render(
+    <NativeBaseProvider initialWindowMetrics={inset}>
+      <HomeScreen navigation={navigation} />
+    </NativeBaseProvider>,
+  );
 
   const title = screen.queryAllByText(/welcome/i);
 
@@ -14,7 +25,11 @@ it("renders without errors", () => {
 });
 
 it("allows navigation to setup page", async () => {
-  const screen = render(<HomeScreen navigation={navigation} />);
+  const screen = render(
+    <NativeBaseProvider initialWindowMetrics={inset}>
+      <HomeScreen navigation={navigation} />
+    </NativeBaseProvider>,
+  );
 
   const startBtn = screen.queryByTestId("get-started-button");
   await fireEvent.press(startBtn);
